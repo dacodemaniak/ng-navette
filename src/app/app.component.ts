b4186a8f339e1af6e6b0360ab40dc14995ae1f1f
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-01-06 12:46:10 +0100

    Simple 2way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'navette';
+export class AppComponent implements OnInit {
+  public appTitle = 'navette';
+
+  public options: string[];
+  public option: string;
+  public height: string = '300px';
+
+  public constructor() {
+    this.options = new Array<string>();
+
+  }
+
+  public ngOnInit(): void {
+    this.options.push('Accueil');
+    this.options.push('Navette');
+    this.options.push('Mon compte');
+    this.options.push('Contact');
+  }
+
+  public addOption(): void {
+      this.options.push(this.option);
+  }
+
+  public checkOption(): boolean {
+    return !(this.option && this.option.trim().length > 0);
+  }
 }
